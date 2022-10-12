fafebd9fc3e89fa097498fb038d202d58f611a81
HighnessAtharva <HighnessAtharva@gmail.com>
2022-10-13 00:55:16 +0530

    cleanup

--- 6/s6a.java
+++ 6/s6a.java
@@ -7,11 +7,15 @@ class s6a {
         System.out.println("Current Thread name : " + S);
         p = t.getPriority();
         System.out.println("Current thread priority : " + p);
+        
+        System.out.println("-------AFTER CHANGE--------");
+        
         t.setName("My Thread");
         S = t.getName();
         System.out.println("Changed Name : " + S);
         t.setPriority(2);
         p = t.getPriority();
         System.out.println("Changed Priority : " + p);
+        
     }
 }
