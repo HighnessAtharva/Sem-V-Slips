13cdf40d8aee6ac745c3fbe0d71b6e7af81d11a8
HighnessAtharva <HighnessAtharva@gmail.com>
2022-10-12 21:46:53 +0530

    16a and 3c added

---
+++ 16/s16a.java
@@ -0,0 +1,46 @@
+
+import java.sql.*;
+
+public class s16a {
+    // static final String DB_URL = "jdbc:mysql://localhost/demo";
+    // static final String USER = "root";
+    // static final String PASS = "";
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        // try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        // Statement stmt = conn.createStatement();)
+        try {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            Connection conn = DriverManager.getConnection("jdbc:odbc:Test");
+            String select_query = "select * from employees";
+
+            // Creating prepared Statement
+            PreparedStatement pstatemnt1 = conn.prepareStatement(select_query, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = pstatemnt1.executeQuery();
+
+            // Moving the cursor to point previous row of third row
+            rs.absolute(3);
+            System.out.println("Cursor is pointing at 3rd row");
+            System.out.println("THIRD ROW \n EMP NUM = " + rs.getInt(1) + "\n LAST NAME = " + rs.getString(2) + "\n FIRST NAME = " + rs.getString(3));
+            
+            // Moving the cursor to point to one row before the current row
+            rs.relative(-1);
+            System.out.println("Cursor is pointing to the 1 row previous to the 3rd row");
+            System.out.println("Second ROW \n EMP NUM = " + rs.getInt(1) + "\n LAST NAME = " + rs.getString(2) + "\n FIRST NAME = " + rs.getString(3));
+            
+            // Moving the cursor to point 4th row after the 2nd row
+            rs.relative(4);
+            System.out.println("Cursor is pointing to the 4th row after the 2nd row");
+            System.out.println("SIXTH ROW \n EMP NUM = " + rs.getInt(1) + "\n LAST NAME = " + rs.getString(2) + "\n FIRST NAME = " + rs.getString(3));
+            
+            // Moving the cursor to point current row
+            System.out.println(" Current Row = " + rs.getRow());
+
+        }
+
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ WebForms/Slip1C/Slip1C/Slip3C.aspx
@@ -0,0 +1,51 @@
+﻿<%--TO RUN THIS PROGRAM SUCCESSFULLY THE FOLLOWING NEEDS TO BE ADDED TO THE WEB.CONFIG FILE IN THE PROJECT
+	<appSettings>
+		<add key="ValidationSettings:UnobtrusiveValidationMode" value="None" />
+	</appSettings>--%>
+
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Slip3C.aspx.cs" %>
+
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+  
+
+</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style3">First value</td>
+                <td class="auto-style2">
+                    <asp:TextBox ID="firstval" runat="server" required="true"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Second value</td>
+                <td>
+                    <asp:TextBox ID="secondval" runat="server"></asp:TextBox>
+                    It should be greater than first value</td>
+            </tr>
+            <tr>
+                <td class="auto-style4"></td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" Text="save" />
+                </td>
+            </tr>
+        </table>
+        <asp:CompareValidator ID="CompareValidator1" runat="server" ControlToCompare="secondval"
+            ControlToValidate="firstval" Display="Dynamic" ErrorMessage="Enter valid value" ForeColor="Red"
+            Operator="LessThan" Type="Integer"></asp:CompareValidator>
+
+        <appsettings>
+            <add key="ValidationSettings:UnobtrusiveValidationMode" value="None" />
+        </appsettings>
+
+    </form>
+</body>
+</html>
