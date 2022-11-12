30a82105ce5b6057f7b42851e1c2c9b6e8e801c7
HighnessAtharva <HighnessAtharva@gmail.com>
2022-11-12 21:53:07 +0530

    rollback s3a

--- 3/s3a.java
+++ 3/s3a.java
@@ -1,28 +1,39 @@
+
 /*Write a java program to display “Hello Java” message n times on the screen. (Use Runnable Interface). */
+
 
 import java.util.Scanner;
 
-class JavaThread extends Thread {
+class JavaThread implements Runnable {
+    Thread t1;
+
+    JavaThread(String s) {
+        t1 = new Thread(this, s);
+        System.out.println("Name of the Thread=" + t1.getName());
+        t1.start();
+    }
+
     public void run() {
         try {
-            System.out.println("Enter No of times to print Hello Java");
+            System.out.println("Enter N");
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
             sc.close();
-            
-            for (int i = 1; i <= n; i++) {
-                System.out.println("Hello Java | Count: " + i);
+            int counter = 1;
+            for (int i = 0; i < n; i++) {
+                System.out.println("Hello Java | Count: " + counter);
                 Thread.sleep(100);
+                counter = counter + 1;
             }
         } catch (InterruptedException e) {
             System.out.println("Exception");
         }
     }
+
 }
 
 public class s3a {
     public static void main(String[] args) {
-        JavaThread jt = new JavaThread();
-        jt.start();
+        JavaThread jt = new JavaThread("Java Thread");
     }
 }
